292e3995a6d723ffb955ef5a2b1e4bbf453e3fcc
Hiba-Saabneh <sabnthbt@gmail.com>
2023-09-11 11:58:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Nav from "./components/Nav/Nav";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+< >
+<div style={{width:"50%",margin:"auto",height:"100vh"}}>
+<Nav/>
+<Home/>
+<Footer/>
+</div>
+</>
+    );
 }
 
 export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div style={{background:"pink",color:"white",fontSize:"25px",textAlign:"center",height:"30vh", marginTop:"10px", 
+display:"flex",
+alignItems:"center"
+,justifyContent:"center"}}>Nav</div>
+  )
+}
