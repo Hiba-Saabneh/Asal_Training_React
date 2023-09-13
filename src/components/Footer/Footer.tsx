28620f4654d796d0098da4bf256d6e4113d96946
Hiba-Saabneh <sabnthbt@gmail.com>
2023-09-13 09:34:15 +0300

    ts_config

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Footer() {
+	return (
+		<div
+			style={{
+				background: 'pink',
+				color: 'white',
+				fontSize: '25px',
+				textAlign: 'center',
+				height: '30vh',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		>
+			Footer
+		</div>
+	)
+}
