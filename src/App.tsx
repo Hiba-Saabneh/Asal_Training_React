7a44d92c20f7112c6fe2aa15eb3464e34f8a9503
Hiba-Saabneh <sabnthbt@gmail.com>
2023-09-14 10:49:29 +0300

    make changes in invertedApp branch

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,27 @@ import React from 'react'
 import InvertedApp from './components/invertComp/InvertedApp'
 
 function App() {
-	let para =
+	let paragragh =
 		'	Lorem ipsum dolor sit amet consectetur adipisicing elit. Id debitis, quos, facilis natus temporibus accusantium et fugiat labore at nihil eius recusandae illo similique provident mollitia nam quod eos nisi quae doloribus eaque ipsa minus! Tenetur quod sint sed veniam omnis quam minima, maxime optio delectus modi officiis quibusdam fugit!'
 	let img = 'img/Image20230913120206.png'
-
-	return (
-		<>
-			<InvertedApp parag={para} img={img} test={true} />
-
-			<InvertedApp parag={para} img={img} test={false} />
-
-			<InvertedApp parag={para} img={img} test={true} />
-		</>
-	)
+	function displayOppositeComponents(n: number) {
+		let components = []
+		let content = ''
+		let positionFlag = false
+		for (var i = 0; i < n; i++) {
+			positionFlag = !positionFlag
+			components.push(
+				<InvertedApp
+					key={i}
+					paragragh={paragragh}
+					img={img}
+					positionFlag={positionFlag}
+				/>
+			)
+		}
+		return components
+	}
+	return <>{displayOppositeComponents(5)}</>
 }
 
 export default App
--- src/components/invertComp/InvertedApp.tsx
+++ src/components/invertComp/InvertedApp.tsx
@@ -1,25 +1,20 @@
 import React from 'react'
-import style from './InvertedApp.module.css'
+import './InvertedApp.css'
 
 interface InvertedAppProps {
-	parag: string
+	paragragh: string
 	img: string
-	test: boolean
+	positionFlag: boolean
 }
 
 export default function InvertedApp(props: InvertedAppProps) {
-	const { parag, img, test } = props
+	const { paragragh, img, positionFlag } = props
+	const OrderStyle = { order: positionFlag ? '0' : '1' }
 	return (
-		<div className={style.inv}>
-			<img
-				src={img}
-				alt=''
-				width='300px'
-				height='250px'
-				style={{ order: test ? '0' : '1' }}
-			/>
+		<div className='InvertedComponent'>
+			<img src={img} alt='' width='300px' height='250px' style={OrderStyle} />
 
-			<p style={{ width: '300px', border: '1px solid black' }}>{parag}</p>
+			<p style={{ width: '300px', border: '1px solid black' }}>{paragragh}</p>
 		</div>
 	)
 }
