8b391f22ba8b9a062c6d9776038b772a1ae696ad
nshaar <nshaar@asaltech.com>
2023-09-14 17:18:10 +0300

    make the code more readable

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,7 @@ import InvertedApp from './components/invertComp/InvertedApp'
 import './App.css'
 
 function App() {
-	const paragragh =
-		'	Lorem ipsum dolor sit amet consectetur adipisicing elit. Id debitis, quos, facilis natus temporibus accusantium et fugiat labore at nihil eius recusandae illo similique provident mollitia nam quod eos nisi quae doloribus eaque ipsa minus! Tenetur quod sint sed veniam omnis quam minima, maxime optio delectus modi officiis quibusdam fugit!'
-	const img = 'img/Image20230913120206.png'
-
-	function displayOppositeComponents(n: number) {
-		let components = Array.from({ length: n }, (_, index) => (
-			<InvertedApp key={index} paragragh={paragragh} img={img} />
-		))
-
-		return components
-	}
-
-	return (
-		<>
-			{displayOppositeComponents(5).map((e, index) => (
-				<div
-					style={{
-						display: 'flex',
-						justifyContent: 'space-around',
-						flexDirection: (index + 1) % 2 == 0 ? 'row-reverse' : 'row',
-					}}
-				>
-					{e}
-				</div>
-			))}
-		</>
-	)
+	return <InvertedApp />
 }
 
 export default App
--- src/components/invertComp/InvertedApp.tsx
+++ src/components/invertComp/InvertedApp.tsx
@@ -1,20 +1,36 @@
 import React from 'react'
 import './InvertedApp.css'
 
-interface InvertedAppProps {
-	paragragh: string
-	img: string
-}
+const data = [
+	{
+		paragragh:
+			'	Lorem ipsum dolor sit amet consectetur adipisicing elit. Id debitis, quos, facilis natus temporibus accusantium et fugiat labore at nihil eius recusandae illo similique provident mollitia nam quod eos nisi quae doloribus eaque ipsa minus! Tenetur quod sint sed veniam omnis quam minima, maxime optio delectus modi officiis quibusdam fugit!',
+		img: 'img/Image20230913120206.png',
+	},
+	{
+		paragragh:
+			'	Lorem ipsum dolor sit amet consectetur adipisicing elit. Id debitis, quos, facilis natus temporibus accusantium et fugiat labore at nihil eius recusandae illo similique provident mollitia nam quod eos nisi quae doloribus eaque ipsa minus! Tenetur quod sint sed veniam omnis quam minima, maxime optio delectus modi officiis quibusdam fugit!',
+		img: 'img/Image20230913120206.png',
+	},
+	{
+		paragragh:
+			'	Lorem ipsum dolor sit amet consectetur adipisicing elit. Id debitis, quos, facilis natus temporibus accusantium et fugiat labore at nihil eius recusandae illo similique provident mollitia nam quod eos nisi quae doloribus eaque ipsa minus! Tenetur quod sint sed veniam omnis quam minima, maxime optio delectus modi officiis quibusdam fugit!',
+		img: 'img/Image20230913120206.png',
+	},
+	{
+		paragragh:
+			'	Lorem ipsum dolor sit amet consectetur adipisicing elit. Id debitis, quos, facilis natus temporibus accusantium et fugiat labore at nihil eius recusandae illo similique provident mollitia nam quod eos nisi quae doloribus eaque ipsa minus! Tenetur quod sint sed veniam omnis quam minima, maxime optio delectus modi officiis quibusdam fugit!',
+		img: 'img/Image20230913120206.png',
+	},
+]
 
-const InvertedApp = (props: InvertedAppProps) => {
-	const { paragragh, img } = props
-	return (
-		<>
-			<img src={img} alt='' width='300px' height='250px' />
-
-			<p style={{ width: '300px', border: '1px solid black' }}>{paragragh}</p>
-		</>
-	)
+const InvertedApp = () => {
+	return data.map((item) => (
+		<div className='container'>
+			<img src={item.img} className='data-image' />
+			<p className='data-parag'>{item.paragragh}</p>
+		</div>
+	))
 }
 
 export default InvertedApp
