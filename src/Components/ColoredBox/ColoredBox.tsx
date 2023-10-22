7898799ee2c8d2457a41ec0b0c327bcb17b8c047
Hiba-Saabneh <sabnthbt@gmail.com>
2023-10-22 11:29:34 +0300

    new changes

--- src/Components/ColoredBox/ColoredBox.tsx
+++ src/Components/ColoredBox/ColoredBox.tsx
@@ -1,83 +1,130 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './ColoredBox.css'
 
 const ColoredBox = () => {
-	const buttonColors = ['red', 'blue', 'green']
-	const guessesColored = ['red', 'red', 'red', 'green', 'blue']
-	const [colorsThatUserChoose, setColorsThatUserChoose] = useState<string[]>([])
-	const [round, setRound] = useState<number>(1)
-	const [endOfGame, setEndOfGame] = useState(false)
-	const resetGame = () => {
+	const [randomColor, setRandomColor] = useState(getRandomColor)
+	const [arrayOfColors, setArrayOfColors] = useState(
+		shuffleArray(getColorSet(randomColor))
+	)
+	const [wrongGuess, setwrongGuess] = useState<(string | number)[]>([])
+	const [round, setRound] = useState(1)
+	const [endOfGame, setEndOFGAme] = useState(false)
+	console.log(randomColor)
+	console.log(wrongGuess)
+
+	//start new arounds of game
+	function startNewGame() {
+		setEndOFGAme(false)
 		setRound(1)
-		setColorsThatUserChoose([])
-		setEndOfGame(false)
+		setwrongGuess([])
+		setRandomColor(getRandomColor)
+		setArrayOfColors(shuffleArray(getColorSet(randomColor)))
 	}
-	const resultOfGame = (ColorsArray: string[]) => {
-		let labelElement = document.getElementById('header')
-		let WrongGuessRound: number[] = []
-		guessesColored.map((guesscolor, index) => {
-			if (guesscolor !== ColorsArray[index]) WrongGuessRound.push(index + 1)
-		})
-		if (WrongGuessRound.length === 0 && labelElement) {
-			labelElement.innerHTML =
-				'<h1 class="correct">All guesses are correct &#9989;<h1>'
-			setEndOfGame(true)
-		} else {
-			if (labelElement)
-				labelElement.innerHTML = `<h1>Try guessing again at round/s : </h1>
-                <div class='wrongRounds'>
-                ${WrongGuessRound.map(
-									(rounded, index) =>
-										`<div key=${index}>${rounded} &#10060; </div>`
-								)} <div>`
-			setEndOfGame(true)
+	//make new color for coloredBox with new color buttons
+	function getNewRound(guessColor: string) {
+		if (guessColor !== randomColor) {
+			setwrongGuess((prevGuess) => {
+				if (prevGuess.length) {
+					return [...prevGuess, round, guessColor]
+				}
+				return [round, guessColor]
+			})
 		}
-	}
-
-	const rightOrWrongGuess = (color: string) => {
-		let ColorsArray = [...colorsThatUserChoose, color]
-		setColorsThatUserChoose([...colorsThatUserChoose, color])
-		if (round < 5) {
+		if (round !== 5) {
 			setRound(round + 1)
+			setRandomColor(getRandomColor)
 		} else {
-			resultOfGame(ColorsArray)
+			setEndOFGAme(true)
 		}
 	}
+	//to randomize the colors in thre cloros array
+	function shuffleArray(arrayOfColors: string[]) {
+		return arrayOfColors.sort(() => Math.random() - 0.5)
+	}
 
+	//to give the random color of the colored box
+	function getRandomColor() {
+		// Generate random values for red, green, and blue to give random color in rgb .
+		const red = Math.floor(Math.random() * 256)
+		const green = Math.floor(Math.random() * 256)
+		const blue = Math.floor(Math.random() * 256)
+
+		// Convert the RGB values to a hexadecimal string
+		let hexColor = `#${red.toString(16)}${green.toString(16)}${blue.toString(
+			16
+		)}`
+
+		return hexColor
+	}
+
+	// give the color set of buttons randomly
+	function getColorSet(randomColor: string) {
+		let arrayOfColors = [randomColor]
+		while (true) {
+			let color = getRandomColor()
+			if (color !== randomColor && arrayOfColors.length < 3) {
+				arrayOfColors.push(color)
+			} else {
+				break
+			}
+		}
+		return arrayOfColors
+	}
+	useEffect(() => {
+		setArrayOfColors(shuffleArray(getColorSet(randomColor)))
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [randomColor])
 	return (
-		<div>
-			<div className={`header ${endOfGame ? 'hide' : 'show'}`}>
-				<h1>Round #{round}</h1>
-			</div>
-			<div
-				className={`header ${endOfGame ? 'header1' : 'header2'} ${
-					!endOfGame ? 'hide' : 'show'
-				}`}
-				id='header'
-			>
-				{endOfGame && (
-					<div className='btnHeader'>
-						<button className='tryBtn' onClick={resetGame}>
-							try again
-						</button>
+		<>
+			<div className={`container `}>
+				<div className={`${endOfGame ? 'displayNone' : 'displayBlock'}`}>
+					<div className='round'>Round #{round}</div>
+					<div
+						className='colorBoxCustom'
+						style={{ background: randomColor }}
+					></div>
+					<div className='btns'>
+						{arrayOfColors.map((color, index) => (
+							<button
+								className='colorButton'
+								key={index}
+								onClick={() => getNewRound(color)}
+							>
+								{color}
+							</button>
+						))}
 					</div>
-				)}
-			</div>
-			<div className={endOfGame ? 'hide' : 'show'}>
-				<div className='colorBox'>Guess the correct color of the box</div>
-				<div className='btnsSpace'>
-					{buttonColors.map((color, index) => (
-						<button
-							className='btns'
-							key={index}
-							onClick={() => rightOrWrongGuess(color)}
-						>
-							{color}
-						</button>
-					))}
+				</div>
+				<div className={`result ${endOfGame ? 'displayBlock' : 'displayNone'}`}>
+					{wrongGuess.length ? (
+						<>
+							<div className='wrongPart'>
+								<h2 className='wrong'>Try guessing again</h2>
+								<div className='wrongAnswers'>
+									{wrongGuess.map((guess, index) =>
+										(index + 1) % 2 !== 0 ? (
+											<span key={index}>Round #{guess}: </span>
+										) : (
+											<>
+												<span key={index}> {guess} is wrong color</span>
+												<br></br>
+											</>
+										)
+									)}
+								</div>
+							</div>
+						</>
+					) : (
+						<>
+							<h2 className='correct'> All guesses are correct !</h2>
+						</>
+					)}
+					<button className='playAgain' onClick={startNewGame}>
+						Play again
+					</button>
 				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
